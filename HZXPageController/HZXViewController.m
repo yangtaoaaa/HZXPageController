426fd1021e0020b381e0cc2ecf6b9b559fa7f290
hezixuan <hezixuan@hzyushi.cn>
2017-11-14 20:36:32 +0800

    +初始化一些配置文件

--- HZXPageController/HZXViewController.m
+++ HZXPageController/HZXViewController.m
@@ -6,23 +6,22 @@
 //  Copyright © 2017年 hezixuan. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "HZXViewController.h"
 
-@interface ViewController ()
+@interface HZXViewController ()
 
 @end
 
-@implementation ViewController
+@implementation HZXViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 
