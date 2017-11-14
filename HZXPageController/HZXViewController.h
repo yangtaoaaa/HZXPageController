a0292c0e957b0c2a81a099f914fcb54af2676a42
hezixuan <hezixuan@hzyushi.cn>
2017-11-14 21:09:05 +0800

    +home tableView

--- HZXPageController/HZXViewController.h
+++ HZXPageController/HZXViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface HZXViewController : UIViewController
+@interface HZXViewController : UITableViewController
 
 
 @end
--- HZXPageController/HZXViewController.m
+++ HZXPageController/HZXViewController.m
@@ -8,7 +8,7 @@
 
 #import "HZXViewController.h"
 
-@interface HZXViewController ()
+@interface HZXViewController ()<UITableViewDelegate, UITableViewDataSource>
 
 @end
 
@@ -17,8 +17,40 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
+    self.tableView.frame = self.view.frame;
+    self.tableView.backgroundColor = [UIColor whiteColor];
+    self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
 }
 
+#pragma mark - tableView DataSource
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellId = @"HZXHomeCellId";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellId];
+        UILabel *rowlabel = [[UILabel alloc] init];
+        rowlabel.frame = CGRectMake(0, 0, 100, 78);
+        rowlabel.backgroundColor = [UIColor greenColor];
+        rowlabel.text = [NSString stringWithFormat:@"第%ld行", indexPath.row];
+        rowlabel.textAlignment = NSTextAlignmentCenter;
+        [cell.contentView addSubview:rowlabel];
+    }
+    return cell;
+}
+
+#pragma mark - tableView delegate
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 80.0;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
