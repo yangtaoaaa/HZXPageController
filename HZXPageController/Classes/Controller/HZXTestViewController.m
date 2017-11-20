4b8343f42eb0db61f7111b08e60c0ba7819bb5e9
何紫璇 <1029989528@qq.com>
2017-11-21 01:37:52 +0800

    *left to middle

--- HZXPageController/Classes/Controller/HZXTestViewController.m
+++ HZXPageController/Classes/Controller/HZXTestViewController.m
@@ -55,7 +55,8 @@
     textLabel.frame = CGRectMake(0, 70, 30, 30);
     textLabel.text = @"2";
     textLabel.textColor = [UIColor redColor];
-    [textView addSubview:textLabel]; self.textView = textView; // 动画开始按钮
+    [textView addSubview:textLabel];
+    self.textView = textView; // 动画开始按钮
     UIButton *beginBtn = [UIButton buttonWithType:UIButtonTypeCustom];
     beginBtn.frame = CGRectMake(0, self.view.height - 60, 80, 30);
     [beginBtn setTitle:@"开始" forState:UIControlStateNormal];
@@ -68,13 +69,39 @@
     [backBtn setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
     [backBtn addTarget:self action:@selector(backBtnClick) forControlEvents:UIControlEventTouchUpInside]; [self.view addSubview:backBtn];
     
+    self.textView.layer.transform = CATransform3DMakeRotation(-M_PI/2, 0, 0, 1);
+    self.textView.layer.position = CGPointMake(self.textView.x-100, self.textView.height*2);
 }
 
--(void)beginBtnClick { [self rightAnimation]; }
+- (void)leftToMiddleAnimation
+{
+    _isDouble = !_isDouble;
+    if (_isDouble) {
+//        self.textView.layer.anchorPoint = CGPointMake(-1.5, 0.5);
+        [UIView animateWithDuration:5.f animations:^{
+            self.textView.layer.transform = CATransform3DMakeRotation(0, 0, 0, 1);
+        }completion:^(BOOL finished) {
+        }];
+    } else {
+        self.textView.layer.position = CGPointMake(self.textView.x - 100, self.textView.height*2);
+        self.textView.layer.anchorPoint = CGPointMake(0.5, 2);
+        [UIView animateWithDuration:5.f animations:^{ self.textView.layer.transform = CATransform3DMakeRotation(0, 0, 0, 1); }completion:^(BOOL finished) {
+        }];
+    }
+}
+
+-(void)beginBtnClick
+{
+//    [self rightAnimation];
+    [self leftToMiddleAnimation];
+}
     
--(void)backBtnClick { [self dismissViewControllerAnimated:YES completion:nil]; }
+-(void)backBtnClick
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
     
-#pragma mark - 向右旋转
+#pragma mark - 中间向右旋转
     
 -(void)rightAnimation
 {
@@ -82,7 +109,9 @@
     if (_isDouble) {
     self.textView.layer.position = CGPointMake(self.textView.x + 50, self.textView.height*2);
     self.textView.layer.anchorPoint = CGPointMake(0.5, 2);
-    [UIView animateWithDuration:5.f animations:^{ self.textView.layer.transform = CATransform3DMakeRotation(M_PI/2, 0, 0, 1); }completion:^(BOOL finished) {
+    [UIView animateWithDuration:5.f animations:^{
+        self.textView.layer.transform = CATransform3DMakeRotation(M_PI/2, 0, 0, 1);
+    }completion:^(BOOL finished) {
 //        self.textView.layer.position = CGPointMake(0, self.textView.height);//self.view.width 让layer的中心点移动到左下角
 //        self.textView.layer.anchorPoint = CGPointMake(0, 1); // 绕着左下角旋转
 //        self.textView.layer.transform = CATransform3DMakeRotation(-M_PI/2, 0, 0, 1);
@@ -100,5 +129,5 @@
 {
     [super didReceiveMemoryWarning];
 }
-        
-        @end
+
+@end
